6668a750a06b88d2a2779b18d0fafcb6bd8f03e4
Mike Glenn <mike.glenn@rammount.com>
2017-01-11 15:44:22 -0500

    working bot from codemash 2017

---
+++ feather-nodebot/README.md
@@ -0,0 +1,5 @@
+# Feather HUZZAH NodeBot
+
+Put Javascript on a Feather because... Javascript!
+
+**Instructions, FAQs, and challenge details for the CodeMash 2017 NodeBots Workshop are available on the [Wiki](https://github.com/hxlnt/feather-nodebot/wiki)!**
---
+++ feather-nodebot/keyupdown.js
@@ -0,0 +1,61 @@
+var EventEmitter = require('events').EventEmitter;
+
+var keypress = require("keypress")
+
+var pressed = false
+var lastPressed = 0
+var keypressCount = 0
+var immediateRpeat = 0 // for debug
+var uptime = 100
+
+var Log = require('log')
+var logLevel = "debug"
+var log = new Log(logLevel)
+
+function keydownup(stream){
+  // keypress handling
+  kp = keypress(stream)
+  stream.on('keypress', function(ch, key){
+    if(pressed == false){
+      stream.emit('keydown', ch, key)
+    }
+    var d = new Date()
+    pressed = true
+    var beforeLastPressed = lastPressed
+    lastPressed = d.getTime()
+    keypressCount++
+  })
+  
+  // immediate
+  function _onImmediate(){
+    if(pressed == false){
+      return
+    }
+    var current = new Date()
+    var interval = current.getTime() - lastPressed
+    if(immediateRpeat % 10000 == 0){
+      log.debug(interval,threshold,keypressCount)
+    }
+    var threshold = uptime
+    // skip first time
+    if(keypressCount == 1){
+      threshold = threshold * 2
+    }
+    if(interval > threshold){
+      log.debug(interval)
+      pressed = false
+      stream.emit('keyup')
+      keypressCount = 0
+
+    }
+    immediateRpeat++ // for debug
+  }
+
+  function onImmediate(){
+    _onImmediate()
+    setImmediate(onImmediate)
+  }
+  setImmediate(onImmediate)
+
+}
+module.exports = keydownup
